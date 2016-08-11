c36ebc38ed38c6e64bb5c2a0124241e0c0c9056f
Wei.Meng <wei.meng@ipinyou.com>
2016-08-11 16:11:25 +0800

    发现服务后缓存，避免每次调用时都需要再次查询

--- ndubbo-client/src/main/java/com/github/dwade/ndubbo/client/zk/ZkServiceDiscovery.java
+++ ndubbo-client/src/main/java/com/github/dwade/ndubbo/client/zk/ZkServiceDiscovery.java
@@ -1,7 +1,9 @@
 package com.github.dwade.ndubbo.client.zk;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
@@ -10,6 +12,7 @@ import org.apache.curator.x.discovery.ServiceDiscovery;
 import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
 import org.apache.curator.x.discovery.ServiceInstance;
 import org.omg.CORBA.ServiceDetail;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.github.dwade.ndubbo.core.discover.IServiceDiscovery;
@@ -19,35 +22,46 @@ import com.github.dwade.ndubbo.core.discover.IServiceDiscovery;
  * @author mengwei
  * 
  */
-public class ZkServiceDiscovery implements IServiceDiscovery {
+@SuppressWarnings("rawtypes")
+public class ZkServiceDiscovery implements IServiceDiscovery, InitializingBean {
 	
 	@Value("${zookeeper.address}")
 	private String zookeeperAddress;
 	
 	@Value("${zookeeper.port}")
 	private int zookeeperPort;
+	
+	private CuratorFramework client;
+	
+	private Map<Class, Collection<ServiceInstance>> serviceList = new ConcurrentHashMap<Class, Collection<ServiceInstance>>();
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@SuppressWarnings({ "unchecked" })
 	@Override
 	public ServiceInstance<?> discoverService(Class<?> interfaceClass) throws Exception {
-		CuratorFramework client = CuratorFrameworkFactory.newClient(getZkConnStr(), new RetryNTimes(5, 1000));
-		client.start();
-
-		String interfaceName = interfaceClass.getName();
-		ServiceDiscovery serviceDiscovery = ServiceDiscoveryBuilder.builder(ServiceDetail.class).client(client)
-				.basePath(interfaceName).build();
-		serviceDiscovery.start();
-
-		Collection<ServiceInstance> services = serviceDiscovery.queryForInstances(interfaceClass.getName());
+		Collection<ServiceInstance> services = serviceList.get(interfaceClass);
+		if(services == null || services.size() == 0) {
+			String interfaceName = interfaceClass.getName();
+			ServiceDiscovery serviceDiscovery = ServiceDiscoveryBuilder.builder(ServiceDetail.class).client(client)
+					.basePath(interfaceName).build();
+			serviceDiscovery.start();
+			services = serviceDiscovery.queryForInstances(interfaceClass.getName());
+			serviceList.put(interfaceClass, services);
+		}
 		if (services != null && services.size() > 0) {
 			int index = new Random().nextInt(services.size());
 			return (ServiceInstance) services.toArray()[index];
 		}
 		return null;
 	}
-
+	
 	private String getZkConnStr() {
 		return zookeeperAddress + ":" + zookeeperPort;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		client = CuratorFrameworkFactory.newClient(getZkConnStr(), new RetryNTimes(5, 1000));
+		client.start();
 	}
 
 }
